453c9babb1b914377b060c72ebed9cbee8050f9c
Kate <ekaterina21051988@yandex.by>
2023-05-29 19:02:53 +0300

    feat: added files

---
+++ index.js
@@ -0,0 +1,8 @@
+import cards from './src/js/cards.js';
+import {createSwitchButton} from './src/js/switchButton.js';
+import {openBurgerMenu} from './src/js/navigation.js';
+import {createCategories} from './src/js/categories.js';
+
+//console.log(cards[0]);
+//console.log(mainPage);
+//console.log(num);
